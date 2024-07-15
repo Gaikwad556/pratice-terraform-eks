a0014503bc0f85160ea1e62d83bd2f9e5d53397a
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-15 14:19:42 +0530

    udpaee

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -4,23 +4,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.25.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0"
-    }
+
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.5.1"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.4"
     }
+
     cloudinit = {
       source  = "hashicorp/cloudinit"
       version = "~> 2.3.2"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23.0"
     }
   }
 
