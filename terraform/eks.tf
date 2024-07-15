30e61a20670a518771383192dd5d0e4e6b674e70
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-15 14:42:13 +0530

    udpate

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -7,7 +7,7 @@ module "eks" {
 
   cluster_endpoint_public_access  = true
 
-    vpc_id                   = module.vpc.id
+    vpc_id                   = module.vpc.vpc_id
   subnet_ids               = [module.vpc.public_subnets]
 
 
