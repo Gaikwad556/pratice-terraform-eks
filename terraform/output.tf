a08d6dd3de01e8b8ba9959b316847640af78819b
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-15 14:47:39 +0530

    update

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,11 +1,11 @@
 output "cluster_endpoint" {
   description = "The endpoint for the EKS cluster"
-  value       = module.eks.cluster_endpoint_public_access
+  value       = module.eks.cluster_endpoint
 }
 
 output "cluster_ca_certificate" {
   description = "The CA certificate for the EKS cluster"
-  value       = module.eks.cluster_ca_certificate.certificate_authority.data
+  value       = module.eks.ca_certificate.certificate_authority.data
 }
 
 output "region" {
