3fa94d1c103dee71e4ef60ac2a8e79002c34d722
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-15 14:30:56 +0530

    update

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 1.6.0"
+  required_version = ">=  1.5.1"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.0"
     }
 
     random = {
