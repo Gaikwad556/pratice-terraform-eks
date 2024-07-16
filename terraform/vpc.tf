6f8a27e6074d5263a5f0b07b5a67fae8db2cdf7e
Gaikwad556 <gamingonearth556@gmail.com>
2024-07-16 14:48:13 +0530

    update

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
 
   name = "vpro_vpc"
   cidr = "10.0.0.0/16"
@@ -9,7 +10,7 @@ module "vpc" {
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
+  enable_dns_hostnames = true
   single_nat_gateway = true
 
   private_subnet_tags = merge(
